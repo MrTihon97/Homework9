4840cad8908631fd7c37bfd5b6b2d11a78189f3f
MrTihon <tihonsomov@mail.ru>
2022-11-13 15:51:13 +0300

    add all

---
+++ Task64/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers(int num1)
+{
+    if (num1 == 0) return;
+    {
+        Console.Write($"{num1} ");
+        NaturalNumbers(num1 - 1);
+    }
+}
+NaturalNumbers(num1);
---
+++ Task66/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Задайте значения M и N. Напишите программу,
+//  которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите целое число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int m, int n)
+{
+    if (m == 0) return (n * (n + 1)) / 2;            // Если M равно нулю
+    else if (n == 0) return (m * (m + 1)) / 2;       // Если N равно нулю
+    else if (m == n) return m;                       // Если M=N
+    else if (m < n) return n + SumNumbers(m, n - 1); // Если M<N
+    else return n + SumNumbers(m, n + 1);            // Если M>N
+}
+Console.WriteLine($"{SumNumbers(m, n)}");
